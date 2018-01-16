16e171a9b4845895c821973145b69f1514152717
Nick Haglind <nhaglind@gmail.com>
2018-01-16 13:08:38 -0600

    spacing

--- dai.js
+++ dai.js
@@ -24,39 +24,39 @@ var walkSync = function(dir, filelist) {
 let images = (walkSync(source));
 
 for (let i = 0; i < images.length; i++)  {
-	AI(images[i]);
+  AI(images[i]);
 }
 
 function AI(fileName) {
-	let image = fileName;
-	let sketchy;
-	request.post({
-		url: 'https://api.deepai.org/api/nsfw-detector',
-		formData: {
-			image: fs.createReadStream(source + image),
-		},
-		headers: {
-			'Api-Key': key
-		}
-	}, function callback(err, httpResponse, body) {
-		if (err) {
-			return console.error('request failed:', err);
-		}
-		var response = JSON.parse(body);
-		var score = parseFloat(response["output"]["nsfw_score"]);
-		(score > threshold) ? sketchy = true: sketchy = false; 
-		(sketchy) ? 
-			console.log(`This is a sketchy image. It has a score of ${score}.`) :
-			console.log(`This image is likely safe. It has a score of ${score}.`);
-		(sketchy) ?
-			fs.copyFile(__dirname + `/${source + image}`, __dirname + `/private/${image}`, (err) => {
-				if (err) throw err;
-				console.log(`Copying ${image} to private.`);
-			})
-				:
-			fs.copyFile(__dirname + `/${source + image}`, __dirname + `/public/${image}`, (err) => {
-				if (err) throw err;
-				console.log(`Copying ${image} to public.`);
-			})
-	});
+  let image = fileName;
+  let sketchy;
+  request.post({
+    url: 'https://api.deepai.org/api/nsfw-detector',
+    formData: {
+      image: fs.createReadStream(source + image),
+    },
+    headers: {
+      'Api-Key': key
+    }
+  }, function callback(err, httpResponse, body) {
+    if (err) {
+      return console.error('request failed:', err);
+    }
+    var response = JSON.parse(body);
+    var score = parseFloat(response["output"]["nsfw_score"]);
+    (score > threshold) ? sketchy = true: sketchy = false; 
+    (sketchy) ? 
+      console.log(`This is a sketchy image. It has a score of ${score}.`) :
+      console.log(`This image is likely safe. It has a score of ${score}.`);
+    (sketchy) ?
+      fs.copyFile(__dirname + `/${source + image}`, __dirname + `/private/${image}`, (err) => {
+        if (err) throw err;
+        console.log(`Copying ${image} to private.`);
+      })
+        :
+      fs.copyFile(__dirname + `/${source + image}`, __dirname + `/public/${image}`, (err) => {
+        if (err) throw err;
+        console.log(`Copying ${image} to public.`);
+      })
+  });
 };
