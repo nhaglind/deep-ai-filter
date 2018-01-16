0dcf4caf15b83f582a98acdc9eb215242e698df7
Nick Haglind <nhaglind@gmail.com>
2018-01-16 12:03:33 -0600

    working loop on directory and function run

--- dai.js
+++ dai.js
@@ -5,35 +5,58 @@ var key = env.api_key;
 
 let threshold = 0.5; 
 let source = './source/' 
-let image = 'wallhaven-547016.jpg';
-let sketchy;
 
-request.post({
-	url: 'https://api.deepai.org/api/nsfw-detector',
-	formData: {
-		image: fs.createReadStream(source + image),
-	},
-	headers: {
-		'Api-Key': key
-	}
-}, function callback(err, httpResponse, body) {
-	if (err) {
-		return console.error('request failed:', err);
-	}
-	var response = JSON.parse(body);
-	var score = parseFloat(response["output"]["nsfw_score"]);
-	(score > threshold) ? sketchy = true: sketchy = false; 
-	(sketchy) ? 
-		console.log(`This is a sketchy image. It has a score of ${score}.`) :
-		console.log(`This image is likely safe. It has a score of ${score}.`);
-	(sketchy) ?
-		fs.copyFile(__dirname + `/${source + image}`, __dirname + `/private/${image}`, (err) => {
-			if (err) throw err;
-			console.log(`Copying ${image} to private.`);
-		})
-			:
-		fs.copyFile(__dirname + `/${source + image}`, __dirname + `/public/${image}`, (err) => {
-			if (err) throw err;
-			console.log(`Copying ${image} to public.`);
-		})
-});
+var walkSync = function(dir, filelist) {
+  var fs = fs || require('fs'),
+      files = fs.readdirSync(dir);
+  filelist = filelist || [];
+  files.forEach(function(file) {
+    if (fs.statSync(dir + file).isDirectory()) {
+      filelist = walkSync(dir + file + '/', filelist);
+    }
+    else {
+      filelist.push(file);
+    }
+  });
+  return filelist;
+};
+
+let images = (walkSync(source));
+
+for (let i = 0; i < images.length; i++)  {
+	AI(images[i]);
+}
+
+function AI(fileName) {
+	let image = fileName;
+	let sketchy;
+	request.post({
+		url: 'https://api.deepai.org/api/nsfw-detector',
+		formData: {
+			image: fs.createReadStream(source + image),
+		},
+		headers: {
+			'Api-Key': key
+		}
+	}, function callback(err, httpResponse, body) {
+		if (err) {
+			return console.error('request failed:', err);
+		}
+		var response = JSON.parse(body);
+		var score = parseFloat(response["output"]["nsfw_score"]);
+		(score > threshold) ? sketchy = true: sketchy = false; 
+		(sketchy) ? 
+			console.log(`This is a sketchy image. It has a score of ${score}.`) :
+			console.log(`This image is likely safe. It has a score of ${score}.`);
+		(sketchy) ?
+			fs.copyFile(__dirname + `/${source + image}`, __dirname + `/private/${image}`, (err) => {
+				if (err) throw err;
+				console.log(`Copying ${image} to private.`);
+			})
+				:
+			fs.copyFile(__dirname + `/${source + image}`, __dirname + `/public/${image}`, (err) => {
+				if (err) throw err;
+				console.log(`Copying ${image} to public.`);
+			})
+	});
+};
